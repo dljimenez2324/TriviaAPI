b3abe7589fb99e05786bdf53fb409f1efdada6ab
dljimenez2324 <dljimenez2324@gmail.com>
2024-04-24 22:47:36 -0700

    TriviaAPI initialization

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+let displayArea = document.getElementById("displayArea");
+let cat1Btn = document.getElementById("cat1Btn");
+let cat2Btn = document.getElementById("cat2Btn");
+let cat3Btn = document.getElementById("cat3Btn");
