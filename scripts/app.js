fc86c307ad606b1d835f509f638e69840f420017
dljimenez2324 <dljimenez2324@gmail.com>
2024-04-25 22:59:12 -0700

    finished styling and final touches to JS for prelim triviaAPI

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 let displayArea = document.getElementById("displayArea");
+let answerArea = document.getElementById("answerArea");
 let cat1Btn = document.getElementById("cat1Btn");
 let cat2Btn = document.getElementById("cat2Btn");
 let cat3Btn = document.getElementById("cat3Btn");
+
 
 cat1Btn.addEventListener("click", function(){
     getM();
@@ -18,6 +20,7 @@ async function getM(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=19").then(Response => Response.json());
     //console.log(apiResponse);
     displayArea.innerText = apiResponse.results["0"].question;
+    answerArea.innerText = "Answer:  " + apiResponse.results["0"].correct_answer;
 }
 
 // API function for Science & Nature
@@ -25,6 +28,7 @@ async function getSN(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=17").then(Response => Response.json());
     //console.log(apiResponse);
     displayArea.innerText = apiResponse.results["0"].question;
+    answerArea.innerText = "Answer:  " + apiResponse.results["0"].correct_answer;
 }
 
 // API function for Cartoons & Animations
@@ -32,4 +36,5 @@ async function getCA(){
     let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=32").then(Response => Response.json());
     //console.log(apiResponse);
     displayArea.innerText = apiResponse.results["0"].question;
+    answerArea.innerText = "Answer:  " + apiResponse.results["0"].correct_answer;
 }
