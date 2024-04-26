67975492ebde636d7333cd1d2dafbaad7b2b1ca0
dljimenez2324 <dljimenez2324@gmail.com>
2024-04-25 21:33:26 -0700

    adding async functionality and modified the html

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,34 @@ let displayArea = document.getElementById("displayArea");
 let cat1Btn = document.getElementById("cat1Btn");
 let cat2Btn = document.getElementById("cat2Btn");
 let cat3Btn = document.getElementById("cat3Btn");
+
+cat1Btn.addEventListener("click", function(){
+    getM();
+})
+cat2Btn.addEventListener("click", function(){
+    getSN();
+})
+cat3Btn.addEventListener("click", function(){
+    getCA();
+})
+
+// API function for Mathematics
+async function getM(){
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=19").then(Response => Response.json());
+    //console.log(apiResponse);
+    displayArea.innerText = apiResponse.results["0"].question;
+}
+
+// API function for Science & Nature
+async function getSN(){
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=17").then(Response => Response.json());
+    //console.log(apiResponse);
+
+}
+
+// API function for Cartoons & Animations
+async function getCA(){
+    let apiResponse = await fetch("https://opentdb.com/api.php?amount=10&category=32").then(Response => Response.json());
+    //console.log(apiResponse);
+
+}
